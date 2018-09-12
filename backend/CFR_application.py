631a2027a7ae11f8832e8116e5aa596e8ad35300
Seojin Kim (Jinne Kim) <34400248+westjinne@users.noreply.github.com>
2018-09-13 02:02:46 +0900

    Update CFR_application.py
    연습 for gender == child, age < 6 check

--- backend/CFR_application.py
+++ backend/CFR_application.py
@@ -20,14 +20,14 @@ def showMain():
 
 @app.route('/main.json/')
 def showJson():
-    with open(os.getcwd()+'/jsons/result_baby4.json') as data_file:
+    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
         data = json.load(data_file)
         return jsonify(data)
 
 
 @app.route('/age.json/')
 def showAgeJson():
-    with open(os.getcwd()+'/jsons/result_baby4.json') as data_file:
+    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
         data = json.load(data_file)
         count = data['info']
         return jsonify(count)
@@ -36,14 +36,14 @@ def showAgeJson():
 
 @app.route('/size.json/')
 def showWidthJson():
-    with open(os.getcwd()+'/jsons/result_baby4.json') as data_file:
+    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
         data = json.load(data_file)
         return jsonify(data['info']['size'])
 
 
 @app.route('/faceCountEx.json/')
 def countFaceCountJson():
-    with open(os.getcwd()+'/jsons/result_baby4.json') as data_file:
+    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
         data = json.load(data_file)
         count = data['info']['faceCount']
         """
@@ -54,6 +54,25 @@ def countFaceCountJson():
         return jsonify(data['faces'])
 
 
+@app.route('/age/')
+def showAgeHTMLJson():
+    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
+        data = json.load(data_file)
+        gender = data['faces'][0]['gender']['value']
+        age = data['faces'][0]['age']['value']
+
+        if((gender == "child") or (int(age[-1:]) < 6)):
+            if(gender == "child"):
+                return render_template('gender.html')
+            else :
+                return render_template('age.html')
+        else:
+            return (a)
+        """
+        if(int(age[-1:]) < 6):
+            return render_template('age.html')
+        """
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8888)
+    app.run(host = '0.0.0.0', port = 5000)
