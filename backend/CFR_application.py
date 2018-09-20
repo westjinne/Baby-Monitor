e897ae6f9ba3dd60ab33fb77be1dff38d063ad6d
Seojin Kim (Jinne Kim) <34400248+westjinne@users.noreply.github.com>
2018-09-20 16:58:21 +0900

    Update CFR_application.py

--- backend/CFR_application.py
+++ backend/CFR_application.py
@@ -13,45 +13,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/main/')
-def showMain():
-    return render_template('first.html')
-
-
 @app.route('/main.json/')
 def showJson():
-    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
+    with open(os.getcwd()+'/json/littleMe2.json') as data_file:
         data = json.load(data_file)
+        gender = data['faces'][0]['gender']['value']
+        age = data['faces'][0]['age']['value']
+
+        if((gender == "child") or (int(age[-1:]) < 6)):
+            if(gender == "child"):
+                return render_template('gender.html')
+            else :
+                return render_template('age.html')
+        else:
+            return ('a')
         return jsonify(data)
-
-
-@app.route('/age.json/')
-def showAgeJson():
-    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
-        data = json.load(data_file)
-        count = data['info']
-        return jsonify(count)
-        #for i in range(count):
-
-
-@app.route('/size.json/')
-def showWidthJson():
-    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
-        data = json.load(data_file)
-        return jsonify(data['info']['size'])
-
-
-@app.route('/faceCountEx.json/')
-def countFaceCountJson():
-    with open(os.getcwd()+'/jsons/result_littleMe2.json') as data_file:
-        data = json.load(data_file)
-        count = data['info']['faceCount']
-        """
-        for i in range(count):
-            print ("-----------------")
-            return jsonify(data['faces'][i])
-        """
-        return jsonify(data['faces'])
 
 
 @app.route('/age/')
